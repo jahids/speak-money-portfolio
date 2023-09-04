0e2003334622e2b268e203d2074a13c115714322
jahids <evanjahid321@gmail.com>
2023-09-04 12:12:49 +0600

    all updated

--- components/MainLayout/MainLayout.js
+++ components/MainLayout/MainLayout.js
@@ -14,11 +14,7 @@ const MainLayout = ({ children }) => {
 					name="viewport"
 					content="width=device-width, initial-scale=1, shrink-to-fit=no"
 				/>
-				<meta name="description" content="" />
-				<meta name="keywords" content="" />
-				<meta name="author" content="Shreethemes" />
-				<meta name="website" content="https://shreethemes.in" />
-				<meta name="email" content="support@shreethemes.in" />
+
 				<meta name="version" content="1.5.0" />
 				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 				<title>Speak money</title>
@@ -28,13 +24,6 @@ const MainLayout = ({ children }) => {
 			{children}
 			<Footer />
 
-			<div className="cookie-popup fixed max-w-lg bottom-3 right-3 left-3 sm:left-0 sm:right-0 mx-auto bg-white dark:bg-slate-900 shadow dark:shadow-gray-800 rounded-md py-5 px-6 z-50">
-				<div className="cookie-popup-actions text-right">
-					<button className="absolute border-none bg-none p-0 cursor-pointer font-semibold top-2 right-2">
-						<i className="uil uil-times text-dark dark:text-slate-200 text-2xl"></i>
-					</button>
-				</div>
-			</div>
 			<a
 				href="#"
 				id="back-to-top"
@@ -49,14 +38,6 @@ const MainLayout = ({ children }) => {
 						className="checkbox opacity-0 absolute"
 						id="chk"
 					/>
-					{/* <label
-            className="label bg-slate-900 dark:bg-white shadow dark:shadow-gray-800 cursor-pointer rounded-full flex justify-between items-center p-1 w-14 h-8"
-            htmlFor="chk"
-          >
-            <i className="uil uil-moon text-[20px] text-yellow-500"></i>
-            <i className="uil uil-sun text-[20px] text-yellow-500"></i>
-            <span className="ball bg-white dark:bg-slate-900 rounded-full absolute top-[2px] left-[2px] w-7 h-7"></span>
-          </label> */}
 				</span>
 			</div>
 			<Script async src="/assets/libs/tiny-slider/min/tiny-slider.js" />
